ef8770cd5d4c0190c32b2f07a6316afd658bbf3c
Blaine <blainesolomon@users.noreply.github.com>
2024-06-23 18:20:06 -0700

    Initial Commit

---
+++ BookMyTime/BookMyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMyTimeApp.swift
+//  BookMyTime
+//
+//  Created by Blaine on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BookMyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
